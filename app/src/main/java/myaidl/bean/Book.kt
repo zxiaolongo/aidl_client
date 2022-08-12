ec267ac86e4c59d0e4bd6a29c9c842447c4ebc57
张小龙 <2247711933@qq.com>
2022-08-12 14:00:56 +0800

    照搬过来aidl有用到bean实体类

---
+++ app/src/main/java/myaidl/bean/Book.kt
@@ -0,0 +1,27 @@
+package com.example.myaidl.bean
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class Book (var name:String?):Parcelable {
+    constructor(parcel: Parcel) : this(parcel.readString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Book> {
+        override fun createFromParcel(parcel: Parcel): Book {
+            return Book(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Book?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
